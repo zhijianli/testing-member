bb6e7b520aa1c2291a986f1a2b9939d478a6f312
zjli_QMjYO <zjli@justhealth.cn>
2024-09-16 23:59:53 +0800

    修改代碼

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -97,7 +97,8 @@ export default {
         progress: true,
         proxy: {
             "/testCenter/*": {
-                target: "http://47.98.124.127:9000", 
+                // target: "http://47.98.124.127:9000", 
+                target: "http://127.0.0.1:9000", 
                 // target: "http://192.168.0.247:8000", //35 80
                 // target: "http://192.168.0.11:8080", //35 80
                 secure: false
@@ -131,7 +132,8 @@ export default {
                 secure: false
             },
             "/orderCenter/*": {
-                target: "http://47.98.124.127:9000", 
+                target: "http://127.0.0.1:9000", 
+                // target: "http://47.98.124.127:9000", 
                 // target: "http://192.168.0.247:8000",
                 secure: false
             },
